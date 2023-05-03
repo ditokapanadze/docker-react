7f7c7b90d821022ab8f9db82f63266f05c1de082
ditokapanadze <66876572+ditokapanadze@users.noreply.github.com>
2023-05-03 17:09:54 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-
-
-// test('fsdfsdfsdf learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = "Test";
+  expect(linkElement).toBe("Test");
+});
